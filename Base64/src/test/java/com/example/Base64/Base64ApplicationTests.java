59be51e08814895f2bcf1476c956ee41e9c6e6a0
attapan2831 <51449252+attapan2831@users.noreply.github.com>
2020-04-30 12:47:01 +0700

    add file
    add new file

---
+++ Base64/src/test/java/com/example/Base64/Base64ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Base64;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Base64ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
